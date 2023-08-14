7c554729d5ca618d0fa198f3495d3afb651b078d
AVHR <viamontea31@gmail.com>
2023-08-13 21:39:06 -0400

    original

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ TODAY = date.today().strftime("%Y-%m-%d")
 
 st.title('Stock Forecast App')
 
-stocks = ('AMC')
+stocks = ('GOOG', 'AAPL', 'MSFT', 'GME')
 selected_stock = st.selectbox('Select dataset for prediction', stocks)
 
 n_years = st.slider('Years of prediction:', 1, 4)
